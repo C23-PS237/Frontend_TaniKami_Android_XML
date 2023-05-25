995f2893039a79532038ba322886eaf54091996d
AktoFredy <workmailAkto@gmail.com>
2023-05-26 04:08:20 +0700

    Bottom Navigation Material ui 3 done

--- app/src/main/java/com/bangkit/tanikami_xml/MainActivity.kt
+++ app/src/main/java/com/bangkit/tanikami_xml/MainActivity.kt
@@ -2,10 +2,34 @@ package com.bangkit.tanikami_xml
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.bangkit.tanikami_xml.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        val navView: BottomNavigationView = binding.navBotView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val appbarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_article, R.id.nav_detection, R.id.nav_profile
+            )
+        )
+
+        setupActionBarWithNavController(navController, appbarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/bangkit/tanikami_xml/home/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.bangkit.tanikami_xml.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bangkit.tanikami_xml.R
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    companion object {
+
+    }
+}
