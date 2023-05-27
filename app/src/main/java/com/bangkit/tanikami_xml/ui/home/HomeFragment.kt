aba88b478158c7806cf90575bc329d1d4490b25f
Jessicasgc <jessicaamadearahma@gmail.com>
2023-05-28 00:58:18 +0700

    Item_products, fragment home, fragment detection, fragment uploading bill

--- app/src/main/java/com/bangkit/tanikami_xml/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bangkit/tanikami_xml/ui/home/HomeFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.bangkit.tanikami_xml.R
 import com.bangkit.tanikami_xml.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -26,9 +24,9 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.toDetail.setOnClickListener {
-            findNavController().navigate(R.id.action_nav_home_to_detailFragment)
-        }
+//        binding.toDetail.setOnClickListener {
+//            findNavController().navigate(R.id.action_nav_home_to_detailFragment)
+//        }
     }
 
     override fun onDestroy() {
