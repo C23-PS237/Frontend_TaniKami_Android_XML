1f91e12939aa1ea2b12d645e5d07841c23bae58c
AktoFredy <workmailAkto@gmail.com>
2023-05-29 04:08:13 +0700

    nav add logout nav, repair profile, but still need data to finalize profile

--- app/src/main/java/com/bangkit/tanikami_xml/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/bangkit/tanikami_xml/ui/profile/ProfileFragment.kt
@@ -5,15 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.bangkit.tanikami_xml.R
+import com.bangkit.tanikami_xml.databinding.FragmentProfileBinding
+
 class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogout.setOnClickListener {
+            findNavController().navigate(R.id.action_nav_profile_to_onBoardingFragment)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
     companion object {
