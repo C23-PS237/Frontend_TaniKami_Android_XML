8a2f93173424a765c6ce96902023de7d62aa293d
AktoFredy <workmailAkto@gmail.com>
2023-05-30 05:17:34 +0700

    splash time done, home product list out but still need repair,

--- app/src/main/java/com/bangkit/tanikami_xml/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bangkit/tanikami_xml/ui/home/HomeFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bangkit.tanikami_xml.R
+import com.bangkit.tanikami_xml.data.helper.Response
+import com.bangkit.tanikami_xml.data.model.Product
 import com.bangkit.tanikami_xml.databinding.FragmentHomeBinding
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,6 +21,7 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private val homeViewModel by viewModels<HomeViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,9 +35,34 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.fabSell.setOnClickListener {
-            findNavController().navigate(R.id.action_nav_home_to_detailFragment)
-//            findNavController().navigate(R.id.action_nav_home_to_detailFragment)
+        showListProducts()
+    }
+
+    private fun showListProducts() {
+        binding.apply {
+            rvProductSell.layoutManager = GridLayoutManager(requireActivity(), 2)
+            homeViewModel.getAllProducts().observe(requireActivity()) {
+                when (it) {
+                    is Response.Loading -> ""
+                    is Response.Error -> {
+                        Snackbar.make(
+                            binding.root,
+                            it.error,
+                            Snackbar.LENGTH_SHORT
+                        ).show()
+                    }
+                    is Response.Success -> {
+                        val listData = HomeProductAdapter(it.data)
+                        rvProductSell.adapter = listData
+
+                        listData.setOnItemClickCallback(object: HomeProductAdapter.OnItemClickCallback {
+                            override fun onProductClicked(data: Product) {
+                                findNavController().navigate(R.id.action_nav_home_to_detailFragment)
+                            }
+                        })
+                    }
+                }
+            }
         }
     }
 
--- app/src/main/java/com/bangkit/tanikami_xml/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/bangkit/tanikami_xml/ui/home/HomeViewModel.kt
@@ -1,4 +1,19 @@
 package com.bangkit.tanikami_xml.ui.home
 
-class HomeViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.bangkit.tanikami_xml.data.helper.Response
+import com.bangkit.tanikami_xml.data.model.Product
+import com.bangkit.tanikami_xml.data.repository.ProductRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val productRepo: ProductRepository
+): ViewModel(){
+
+    fun getAllProducts() : LiveData<Response<List<Product>>> {
+        return productRepo.getAllProduct()
+    }
 }
