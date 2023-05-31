0ec6c9f9e36bd475dcfb399a45d52c5bc73b6b52
Jessicasgc <jessicaamadearahma@gmail.com>
2023-05-31 16:43:38 +0700

    Add Edit Product Fragment and Add Edit Product XML and the Navigation from Detail Fragment to Edit Product Fragment

--- app/src/main/java/com/bangkit/tanikami_xml/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/bangkit/tanikami_xml/ui/detail/DetailFragment.kt
@@ -5,18 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.bangkit.tanikami_xml.R
+import com.bangkit.tanikami_xml.databinding.FragmentAddProductBinding
+import com.bangkit.tanikami_xml.databinding.FragmentDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailFragment : Fragment() {
 
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fabEditDetail.setOnClickListener{
+            findNavController().navigate(R.id.action_detailFragment_to_editProductFragment)
+        }
     }
 
     companion object {
