d40595c8922f7904872a616e89ec1963c12f51b0
Jessicasgc <jessicaamadearahma@gmail.com>
2023-06-02 01:49:10 +0700

    Add navigation and navgraph for ConfirmAddProductFragment to history, history Fragment to detail, edit item_purchase_history

--- app/src/main/java/com/bangkit/tanikami_xml/ui/history/buying_history/HistoryBuyFragment.kt
+++ app/src/main/java/com/bangkit/tanikami_xml/ui/history/buying_history/HistoryBuyFragment.kt
@@ -5,20 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.bangkit.tanikami_xml.R
+import com.bangkit.tanikami_xml.databinding.FragmentHistoryBuyBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HistoryBuyFragment : Fragment() {
-
+    private var _binding: FragmentHistoryBuyBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history_buy, container, false)
+        _binding = FragmentHistoryBuyBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.rvPurchaseHistory.setOnClickListener {
+            findNavController().navigate(R.id.action_historyBuyFragment_to_detailFragment)
+        }
+    }
     companion object {
     }
 }
--- app/src/main/java/com/bangkit/tanikami_xml/ui/history/selling_history/HistorySellFragment.kt
+++ app/src/main/java/com/bangkit/tanikami_xml/ui/history/selling_history/HistorySellFragment.kt
@@ -5,20 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.bangkit.tanikami_xml.R
+import com.bangkit.tanikami_xml.databinding.FragmentHistorySellBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HistorySellFragment : Fragment() {
 
+    private var _binding:FragmentHistorySellBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history_sell, container, false)
+        _binding = FragmentHistorySellBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.rvSalesHistory.setOnClickListener {
+            findNavController().navigate(R.id.action_historySellFragment_to_detailFragment)
+        }
+    }
     companion object {
     }
 }
