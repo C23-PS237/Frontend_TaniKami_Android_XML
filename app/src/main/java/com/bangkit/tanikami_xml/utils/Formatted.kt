ff07ddc6d8b09d1b2fb073bbb0b8bc014522c272
AktoFredy <workmailAkto@gmail.com>
2023-06-04 02:06:40 +0700

    idr add space done, detection animation done.

--- app/src/main/java/com/bangkit/tanikami_xml/utils/Formatted.kt
+++ app/src/main/java/com/bangkit/tanikami_xml/utils/Formatted.kt
@@ -3,12 +3,15 @@ package com.bangkit.tanikami_xml.utils
 import java.text.NumberFormat
 import java.util.Currency
 import java.util.Locale
+import kotlin.text.StringBuilder
 
 object Formatted {
     fun formatIDRCurrency(price: Int): String {
         val format = NumberFormat.getCurrencyInstance(Locale("id", "ID"))
         format.currency = Currency.getInstance("IDR")
 
-        return format.format(price)
+        return addSpaceToIndonesianRupiah(format.format(price))
     }
+
+    private fun addSpaceToIndonesianRupiah(value: String): String = StringBuilder(value).insert(2, ". ").toString()
 }
