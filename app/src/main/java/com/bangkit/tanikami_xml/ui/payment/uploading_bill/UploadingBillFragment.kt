04c9f8abe23f4167ae66459fa85bd2d03b29fb40
Jessicasgc <jessicaamadearahma@gmail.com>
2023-06-09 18:28:50 +0700

    Cancel using CameraX in Uploading Bill Frament and use Intent Camera for UploadingBillFragment

--- app/src/main/java/com/bangkit/tanikami_xml/ui/payment/uploading_bill/UploadingBillFragment.kt
+++ app/src/main/java/com/bangkit/tanikami_xml/ui/payment/uploading_bill/UploadingBillFragment.kt
@@ -1,25 +1,21 @@
 package com.bangkit.tanikami_xml.ui.payment.uploading_bill
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.bangkit.tanikami_xml.CameraFragment
 import com.bangkit.tanikami_xml.R
+import com.bangkit.tanikami_xml.createCustomTempFile
 import com.bangkit.tanikami_xml.databinding.FragmentUploadingBillBinding
-import com.bangkit.tanikami_xml.rotateFile
 import com.bangkit.tanikami_xml.uriToFile
 import dagger.hilt.android.AndroidEntryPoint
 import java.io.File
@@ -30,28 +26,8 @@ class UploadingBillFragment : Fragment() {
     private var _binding: FragmentUploadingBillBinding?=null
     private val binding get() = _binding!!
     private var getFile: File? = null
+    private lateinit var currentPhotoPath: String
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (!allPermissionsGranted()) {
-                Toast.makeText(
-                    requireActivity(),
-                    "Tidak mendapatkan permission.",
-                    Toast.LENGTH_SHORT
-                ).show()
-                activity?.finish()
-            }
-        }
-    }
-
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
-    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -65,35 +41,38 @@ class UploadingBillFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.btnSubmit.setOnClickListener {
+
             findNavController().navigate(R.id.action_uploadingBillFragment_to_paymentsStepFragment)
         }
-        binding.btnCamera.setOnClickListener { startCameraX() }
+        binding.btnCamera.setOnClickListener { startTakePhoto() }
         binding.btnGallery.setOnClickListener {
             startGallery()
         }
     }
 
-    private fun startCameraX() {
-//        val intent = Intent(requireActivity(), CameraFragment::class.java)
-//        launcherIntentCameraX.launch(intent)
-        findNavController().navigate(R.id.action_uploadingBillFragment_to_cameraFragment)
-    }
+    private fun startTakePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(requireActivity().packageManager)
 
-    private val launcherIntentCameraX = registerForActivityResult(
+        createCustomTempFile(requireContext()).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                requireActivity(),
+                "com.bangkit.tanikami_xml",
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
+    }
+    private val launcherIntentCamera = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) {
-        if (it.resultCode == CAMERA_X_RESULT) {
-            val myFile = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                it.data?.getSerializableExtra("picture", File::class.java)
-            } else {
-                @Suppress("DEPRECATION")
-                it.data?.getSerializableExtra("picture")
-            } as? File
-
-            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
-
-            myFile?.let { file ->
-                rotateFile(file, isBackCamera)
+        if (it.resultCode == AppCompatActivity.RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+            myFile.let { file ->
+//              Silakan gunakan kode ini jika mengalami perubahan rotasi
+//              rotateFile(file)
                 getFile = file
                 binding.ivBill.setImageBitmap(BitmapFactory.decodeFile(file.path))
             }
@@ -106,6 +85,8 @@ class UploadingBillFragment : Fragment() {
         val chooser = Intent.createChooser(intent, "Choose a Picture")
         launcherIntentGallery.launch(chooser)
     }
+
+
     private val launcherIntentGallery = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result ->
@@ -120,11 +101,10 @@ class UploadingBillFragment : Fragment() {
     }
 
 
-    companion object {
-        const val CAMERA_X_RESULT = 200
 
-        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
-        private const val REQUEST_CODE_PERMISSIONS = 10
+
+    companion object {
+
     }
 
 }
